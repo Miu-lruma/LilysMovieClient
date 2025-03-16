f0bf63a9c6798ba989f22f6a666f4e3cd418f887
Miu-lruma <lkviice@gmail.com>
2025-03-16 01:52:53 -0500

    Pls

--- lilysmovieclient/vite.config.ts
+++ lilysmovieclient/vite.config.ts
@@ -3,8 +3,9 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/LilysMovieClient",
     plugins: [plugin()],
     server: {
         port: 65031,
     }
-})
+});
